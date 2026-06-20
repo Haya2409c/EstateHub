364332e37c3bfaf4aa3a838a5aef10c26964cf82
Shakil Chhotani <shonni.munni@gmail.com>
2026-06-20 11:20:41 +0500

    Remove old unused folders, keep only RealtorsPortal backend 20-06-2026

--- RealtorsPortal backend/RealtorsPortal/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace RealtorsPortal.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
